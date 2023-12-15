bee9ebb4bb7af6959a6e4f323febaf7759911163
Jhosett <darwinjhosettbermudezromero@gmail.com>
2023-12-15 11:23:29 -0500

    empezando practica

---
+++ practicaExamen2/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="mx-[25%] my-[5%]">
+
+    <div class="bg-blue-200 p-5">
+      <p class="my-2 font-bold text-center text-2xl">CARRITO DE COMPRAS</p>
+      <img class="w-[40%] h-[30%] mx-auto rounded-lg mb-3" src="https://m.media-amazon.com/images/I/615HyRBj-FL._AC_SY500_.jpg" alt="">
+    </div>
+
+  </div>
+</template>
+
+
