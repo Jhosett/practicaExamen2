9c7d9806f16fc8a22658210ee008890e721ebe67
Jhosett <darwinjhosettbermudezromero@gmail.com>
2023-12-16 10:51:18 -0500

    Diseño terminado

--- practicaExamen2/src/App.vue
+++ practicaExamen2/src/App.vue
@@ -1,5 +1,16 @@
 <script setup>
 
+const productos = 
+{
+
+  nombre: 'Tenis casuales',
+  subTotal: '120000',
+  envio: '15000'
+
+}
+
+let cont
+
 </script>
 
 <template>
@@ -10,13 +21,13 @@
       <img class="w-[60%] h-[30%] mx-auto rounded-lg mb-3" src="https://m.media-amazon.com/images/I/615HyRBj-FL._AC_SY500_.jpg" alt="">
       <ul class="mb-4 text-center">
         <li>
-
+          
         </li>
       </ul>
       <button class="bg-green-600 text-white font-semibold ml-[58%] py-1 px-3 text-lg rounded-md hover:bg-white hover:text-green-600 cursor-pointer">Continuar</button>
     </div>
 
-    <div class="bg-blue-200 p-8 mx-auto w-[50%]">
+    <div class="bg-blue-200 p-8 mx-auto w-[50%] mb-10">
       <p class="mt-2 mb-6 font-bold text-2xl">Datos del comprador</p>
       <label class="font-semibold text-gray-700 text-lg" for="nombre">Nombres y apellidos</label>
       <input class="py-1 w-[100%] pl-2 bg-slate-200 rounded-md mb-3" type="text" id="nombre">
@@ -31,7 +42,16 @@
       <input class="py-1 w-[100%] pl-2 bg-slate-200 rounded-md mb-3" type="text" id="mPago">
 
       <button class="bg-green-600 text-white font-semibold ml-[66%] py-1 px-3 text-lg rounded-md hover:bg-white hover:text-green-600 cursor-pointer">Ordenar</button>
+    </div>
 
+    <div class="bg-blue-200 p-8 mx-auto w-[50%]">
+      <p class="mt-2 mb-6 font-bold text-2xl">Resumen de la compra</p>
+      <ul class="mb-4 text-center">
+        <li>
+
+        </li>
+      </ul>
+      <button class="bg-green-600 text-white font-semibold ml-[72%] py-1 px-3 text-lg rounded-md hover:bg-white hover:text-green-600 cursor-pointer">Pagar</button>
     </div>
 
   </div>
